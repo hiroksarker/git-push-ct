4be3dbc34d824ea8426d72d3fb5d3769ecefede7
Hirok Sarker <hirok.sarker@gmail.com>
2015-08-20 14:12:09 +0600

    JS Change & CSS
    Change JS & CSS

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -6,29 +6,29 @@ for (var i = 0; i < keys.length; i++) {
         var input = document.querySelector('.screen');
         var inputVal = input.innerHTML;
         var btnVal = this.innerHTML;
-        if (btnVal == 'C') {
+        if (btnVal === 'C') {
             input.innerHTML = '';
             decimalAdded = false;
-        } else if (btnVal == '=') {
+        } else if (btnVal === '=') {
             var equation = inputVal;
             var lastChar = equation[equation.length - 1];
             equation = equation.replace(/x/g, '*').replace(/÷/g, '/');
-            if (operators.indexOf(lastChar) > -1 || lastChar == '.')
+            if (operators.indexOf(lastChar) > -1 || lastChar === '.')
                 equation = equation.replace(/.$/, '');
             if (equation)
                 input.innerHTML = (new Function('return ' + equation))();
             decimalAdded = false;
         } else if (operators.indexOf(btnVal) > -1) {
             var lastChar = inputVal[inputVal.length - 1];
-            if (inputVal !== '' && operators.indexOf(lastChar) == -1)
+            if (inputVal !== '' && operators.indexOf(lastChar) === -1)
                 input.innerHTML += btnVal;
-            else if (inputVal === '' && btnVal == '-')
+            else if (inputVal === '' && btnVal === '-')
                 input.innerHTML += btnVal;
             if (operators.indexOf(lastChar) > -1 && inputVal.length > 1) {
                 input.innerHTML = inputVal.replace(/.$/, btnVal);
             }
             decimalAdded = false;
-        } else if (btnVal == '.') {
+        } else if (btnVal === '.') {
             if (!decimalAdded) {
                 input.innerHTML += btnVal;
                 decimalAdded = true;
